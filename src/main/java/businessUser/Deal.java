f536a3b67ae6177129d0fabc3bb9f36521a753f7
yesh-sharma <yesh@zasyasolutions.com>
2025-05-28 11:58:56 +0530

    changes done in deals class

--- src/main/java/businessUser/Deal.java
+++ src/main/java/businessUser/Deal.java
@@ -24,19 +24,14 @@ public class Deal extends Basetest {
 
 	static int initialRemainingDealCountOnDealsDashboard;
 	static int initialActiveDealCountOnDealsDashboard;
-
-	
 	@Test(priority = 1)
-    public void createdealBybusinessUserAndAdminApprovesTheDeal() throws InterruptedException {
+	public void createdealBybusinessUserAndAdminApprovesTheDeal() throws InterruptedException {
 
 		loginApplication();
 		// get the deal count
-		
-		WebElement remainingDealCountElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='display-5'])[1]")));
-
+		WebElement remainingDealCountElement = driver.findElement(By.xpath("(//div[@class='display-5'])[1]"));
 		initialRemainingDealCountOnDashboard = Integer.parseInt(remainingDealCountElement.getText());
 		System.out.println("Initial Deal Count on dashboard: " + initialRemainingDealCountOnDashboard);
-
 
 		// GET TOTAL CLAIM TEXT
 		WebElement TotalClaimedCountBox = driver.findElement(By.xpath("(//div[@class='display-5 mt-2']) [1]"));
@@ -73,7 +68,7 @@ public class Deal extends Basetest {
 	}
 
 	// @Test(priority = 4)
-	   public void createdealBybusinessUserAndAdminDeclineThedealWithReasonForDecline() throws InterruptedException {
+	public void createdealBybusinessUserAndAdminDeclineThedealWithReasonForDecline() throws InterruptedException {
 
 		loginApplication();
 		ReuseableCode reuse = new ReuseableCode(driver);
@@ -114,8 +109,8 @@ public class Deal extends Basetest {
 
 	}
 
-	// @Test(priority = 2, dependsOnMethods = {
-	// "createdealBybusinessUserAndAdminApprovesTheDeal" })
+//	 @Test(priority = 2, dependsOnMethods = {
+//	 "createdealBybusinessUserAndAdminApprovesTheDeal" })
 	public void dealCreatedNowCheckTheMainDashboardDealsCountAndAfterThatGoToDealDashboardAndCheckTheRemainingDealCountAndActiveDealcount()
 			throws InterruptedException {
 
